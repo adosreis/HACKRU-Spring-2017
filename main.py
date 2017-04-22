b199b215269446b67ecc44ce48e0135182ee4b70
Andrew Dos Reis <andrewdos2@gmail.com>
2017-04-22 16:42:36 -0400

    init commit, added some files to work with

---
+++ main.py
@@ -0,0 +1,14 @@
+import xml.etree.ElementTree as ET
+
+def game(a):
+    hero_name = a
+    print("You arise ")
+
+if __name__ == '__main__':
+    myList = ["Y","y","yes","YES","Yes","No","N","NO","no","n"]
+    con = ""
+    while(not myList.Contains(con)):
+        con = input("Continue? Y/N?")
+    if myList[]
+
+    game(input("What is your name?"))
