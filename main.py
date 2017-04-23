14b9ac12e6c4c0349617c5966e356c6ae88c11e6
Dom <dwirkijowski@gmail.com>
2017-04-23 10:49:26 -0400

    added synopsis

--- main.py
+++ main.py
@@ -6,7 +6,15 @@ from utils import clear
 
 
 if __name__ == '__main__':
-    print("Escape: New Jersey \n")
+    print("Escape: New Jersey\n"
+          "Well, that sucks! Looks like you're stuck in SUICIDE HEIGHTS, in the dreaded state NEW JERSEY. It can't be too hard to escape, right? \n"
+          "Unfortunately, you're horribly mistaken.  You'll probably encounter a series of EVENTS that unwillingly happen to you and your party of THREE RELATIVELY NONDESCRIPT COMPANIONS. \n "
+          "All you have to do is decide which unfortunate thing you want to have happen. There are different possible outcomes for each choice you make depending on the LIKELIHOOD of your success,\n"
+          " based on whatever RELEVENT STAT is applicable to the situation and a RANDOMLY GENERATED NUMBER from the opponent. That's right. You're fighting numbers. Can you win and find your way home? \n"
+          "... Well, no you can't. This is a trial version. As of right now, escape is just a pipe dream, and you're just playing for the sadistic amusement. \n"
+          "Enjoy!\n"
+          "- Dev. Team \n")
+
     root = XML()
     con = ""
     while(not (con in binary_answer_list)):
